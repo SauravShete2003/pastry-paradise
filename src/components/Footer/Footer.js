09820b4cd5cf096dd055f95c5d6a6960fc460dba
sauravshete2003 <sauravshete72@gmail.com>
2024-07-13 10:31:56 +0530

    alt

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,11 +6,12 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer-container">
-      <img src={logo} className="pastry-paradise-logo" />
+      <img src={logo} className="pastry-paradise-logo" alt="logo"/>
       <div className="links-container">
         <a href="https://www.instagram.com/saurav_shete_patil/">
           <img
             src={insta}
+            alt="instagram"
             className="
       social-media-icon"
           />
@@ -18,6 +19,7 @@ function Footer() {
         <a href="https://m.facebook.com/saurav.shete.10/">
           <img
             src={facebook}
+            alt="facebook"
             className="
       social-media-icon"
           />
@@ -25,6 +27,7 @@ function Footer() {
         <a href="https://www.linkedin.com/in/saurav-shete/">
           <img
             src={linkedin}
+            alt="linkedin"
             className="
       social-media-icon"
           />
@@ -33,6 +36,7 @@ function Footer() {
           <img
             src="https://media.licdn.com/dms/image/C4D0BAQGjCtiL8FcUcw/company-logo_200_200/0/1646835555233/peerlist_logo?e=2147483647&v=beta&t=5-raV6b7AI36OcSWiuqE6TrGkaNehuqRQa9OYwobaLs"
             className="social-media-icon"
+            alt="peerlist"
           />
         </a>
       </div>
